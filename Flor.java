72f986116545e7a6fd89b63827d91c8033300843
dangelo26leon <dangelo26leon@gmail.com>
2025-09-12 09:31:01 -0500

    Update Flor.java

--- Flor.java
+++ Flor.java
@@ -10,7 +10,7 @@ public class Flor extends JPanel {
         g.fillOval(50, 100, 100, 100); // pétalo izquierdo
         g.fillOval(150, 100, 100, 100); // pétalo derecho
         g.fillOval(100, 150, 100, 100); // pétalo inferior
-        g.setColor(Color.GREEN);
+        g.setColor(Color.BLUE);
         g.fillOval(110, 110, 80, 80);   // centro
     }
 
