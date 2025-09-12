aa58dbd13daa23f3780a058f678c65dd88905bbb
Dangelo Leon <leonvalderramadangeloalexander@gmail.com>
2025-09-12 09:16:37 -0500

    Añadir programa de Flor

---
+++ Flor.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Flor extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.YELLOW);
+        g.fillOval(100, 50, 100, 100); // pétalo superior
+        g.fillOval(50, 100, 100, 100); // pétalo izquierdo
+        g.fillOval(150, 100, 100, 100); // pétalo derecho
+        g.fillOval(100, 150, 100, 100); // pétalo inferior
+        g.setColor(Color.ORANGE);
+        g.fillOval(110, 110, 80, 80);   // centro
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flor Amarilla 🌼");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 350);
+        frame.add(new Flor());
+        frame.setVisible(true);
+    }
+}
