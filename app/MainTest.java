0dfe489d2be1e6fdecd7caf30590807db1212608
dangelo26leon <dangeloleon75@gmail.com>
2025-11-07 09:59:32 -0500

    agregar

--- app/MainTest.java
+++ app/MainTest.java
@@ -1,3 +1,12 @@
 public class MainTest {
+    public static void main(String[] args) {
+        boolean resultado = true;
 
+        if (resultado) {
+            System.out.println("TEST OK");
+        } else {
+            System.out.println("TEST FAIL");
+            System.exit(1); 
+        }
+    }
 }
